3832a0c6cdaa2a94911b8f7a932cc55c649f8ede
unknown <meriemelmecaniqgmail.com>
2025-07-10 17:55:20 +0200

    hdhd

--- app/Models/CommandeProduit.php
+++ app/Models/CommandeProduit.php
@@ -7,5 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class CommandeProduit extends Model
 {
     protected $table='commande_produits';
+    
     protected $fillable=['Commande_id','Product_id','quantity','price'];
 }
